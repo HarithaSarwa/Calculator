49ca9496f255a35410ee7c9f9d1402137ab356b5
HarithaSarwa <haritha.sarwa@outlook.com>
2020-03-09 23:11:21 +0530

    Added Addition operation

--- src/calculator/ArithmeticCalculator.java
+++ src/calculator/ArithmeticCalculator.java
@@ -1,5 +1,16 @@
 package calculator;
 
 public class ArithmeticCalculator {
-
+	public static void main(String srgs[]) {
+		
+		int a=10,b=20,sum;
+		sum = a+b;
+		
+		System.out.println("Addition operation");
+		System.out.println("Sum of a and b is " +sum);
+		
+		
+		
+	}
+	
 }
